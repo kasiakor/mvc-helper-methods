1d20c93a4e366e0a7c87bf378e7d6de2d4ced481
kasia.kordys <kasia.kordys@gmail.com>
2020-11-11 19:52:15 +0000

    Add project files.

---
+++ HelperMethods/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HelperMethods.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewBag.Fruit = new string[] { "Pear","Plum","Pomelo"};
+            ViewBag.Cities = new string[] { "Berlin", "Vienna", "Innsbruck" };
+            string message = "This is my html element <input>";
+            return View((object)message);
+        }
+    }
+}
