1e594d985a5a88d67b6eee0c98355c810927585d
kasia.kordys <kasia.kordys@gmail.com>
2020-11-19 19:11:19 +0000

    add people controller and table style

---
+++ HelperMethods/Controllers/PeopleController.cs
@@ -0,0 +1,44 @@
+﻿using HelperMethods.Models;
+using System;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace HelperMethods.Controllers
+{
+    public class PeopleController : Controller
+    {
+
+        private Person[] personData =
+        {
+            new Person {FirstName = "John", LastName = "Doe", Role = Role.Admin},
+            new Person {FirstName = "Sarah", LastName = "Key", Role = Role.User},
+            new Person {FirstName = "Boris", LastName = "Low", Role = Role.Guest},
+            new Person {FirstName = "Kate", LastName = "Bush", Role = Role.User}
+        };
+        // GET: People
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetPeople()
+        {
+            return View(personData);
+        }
+
+        [HttpPost]
+        public ActionResult GetPeople(string selectedRole)
+        {
+
+            if (selectedRole == null || selectedRole == "All")
+            {
+                return View(personData);
+            }
+            else
+            {
+                Role selected = (Role)Enum.Parse(typeof(Role), selectedRole);
+                return View(personData.Where(p =>p.Role == selected));
+            }
+        }
+    }
+}
